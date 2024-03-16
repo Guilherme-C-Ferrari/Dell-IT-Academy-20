88283d7b84e67585241c9cd167ec0e671b07ca63
Guilherme Carvalho Ferrari <guicferrari@hotmail.com>
2024-03-16 03:35:59 -0300

    Bookmaker first implementation.

--- src/Header Files/Bookmaker.h
+++ src/Header Files/Bookmaker.h
@@ -1,1 +1,22 @@
 #pragma once
+# include "Edition.h"
+
+class Bookmaker {
+
+private:
+
+	vector<Edition> Editions;
+
+public:
+
+	Bookmaker();
+
+	// Método que inicializa o programa
+	void Start();
+
+	// Método que roda o programa, onde fica o menu que será usado pelo usuário para acessar as funcionalidades
+	void Run();
+
+	// Método que finaliza o programa
+	void Finish();
+};
---
+++ src/Source Files/Bookmaker.cpp
@@ -0,0 +1,9 @@
+#include "../Header Files/Bookmaker.h"
+
+Bookmaker::Bookmaker() {}
+
+void Bookmaker::Run() {}
+
+void Bookmaker::Start() {}
+
+void Bookmaker::Finish() {}
