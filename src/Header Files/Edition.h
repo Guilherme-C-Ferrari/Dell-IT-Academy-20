17a9fa1edfff69b41196e0856801d2e19a95ced4
Guilherme Carvalho Ferrari <guicferrari@hotmail.com>
2024-03-18 01:56:16 -0300

    Update Editor Functions.

--- src/Header Files/Edition.h
+++ src/Header Files/Edition.h
@@ -6,11 +6,22 @@ class Edition {
 private:
 
 	int edition_number;
-	vector<Bet> bets;
+	vector<Bet> bets = vector<Bet>(0);
 	vector<int> winner_numbers = vector<int>(5);
-	Bettor winner;
+	vector<Bettor> winner = vector<Bettor>(0);
 
 public:
-
 	Edition();
+
+	void RegisterNewBet(string name, string cpf, int option, int n[5]);
+
+	void ShowBets();
+
+	void ExecuteDrawPhase();
+
+	void ExecuteCoutingPhase();
+
+	void ExecuteAwardPhase();
+
+	void ShowEditionData();
 };
--- src/Source Files/Edition.cpp
+++ src/Source Files/Edition.cpp
@@ -1,3 +1,56 @@
 #include "../Header Files/Edition.h"
 
-Edition::Edition() {}
+Edition::Edition() {
+	edition_number = 0;
+	bets.resize(0);
+}
+
+void Edition::RegisterNewBet(string name, string cpf, int option, int n[5]) {
+	int indexBet = bets.size();
+	bets.resize(indexBet + 1);
+
+	bets[indexBet].SetCode(1000 + indexBet);
+	if (option == 1) {
+		bets[indexBet].SetNumbers(n);
+	}
+	else if (option == 2) {
+		bets[indexBet].SetNumbersRandomly();
+	}
+	bets[indexBet].SetBettor(name, cpf);
+}
+
+void Edition::ShowBets() {
+	Bettor tempBettor;
+	int* tempArray;
+	for (int i = 0; i < bets.size(); i++) {
+		tempBettor = bets[i].GetBettor();
+		tempArray = bets[i].GetNumbers();
+		cout << "\nAposta número: " << bets[i].GetCode();
+		cout << "\nApostador: " << tempBettor.GetName() << " - CPF: " << tempBettor.GetCPF();
+		cout << "\nNúmeros: ";
+		for (int j = 0; j < 5; j++) {
+			cout << tempArray[j] << " ";
+		}
+		cout << endl;
+	}
+
+	if (bets.size() == 0) {
+		cout << "\nNenhuma aposta registrada.\n";
+	}
+}
+
+void Edition::ExecuteDrawPhase() {
+
+}
+
+void Edition::ExecuteCoutingPhase() {
+
+}
+
+void Edition::ExecuteAwardPhase() {
+
+}
+
+void Edition::ShowEditionData() {
+
+}
