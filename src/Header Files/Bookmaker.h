7f71f97803b2e20f0cd4a8dc78c3d2058f9a995a
Guilherme Carvalho Ferrari <guicferrari@hotmail.com>
2024-03-18 01:56:32 -0300

    Update Bookmaker Functions.

--- src/Header Files/Bookmaker.h
+++ src/Header Files/Bookmaker.h
@@ -5,18 +5,39 @@ class Bookmaker {
 
 private:
 
-	vector<Edition> Editions;
+	vector<Edition> editions;
 
 public:
 
 	Bookmaker();
 
-	// Método que inicializa o programa
+	// Método que inicializa o programa.
 	void Start();
 
-	// Método que roda o programa, onde fica o menu que será usado pelo usuário para acessar as funcionalidades
+	// Método que roda o programa, onde fica o menu que será usado pelo usuário para acessar as funcionalidades.
 	void Run();
 
-	// Método que finaliza o programa
+	// Método que finaliza o programa.
 	void Finish();
+
+	// Método que testa exceções.
+	int TestException(string arg_string);
+	
+	// Método que inicia uma nova edição.
+	void StartNewEdition();
+
+	// Método que inicia a fase de edição.
+	void StartBetPhaseOnEdition(int indexEdition);
+
+	// Método que inicia a fase de sorteio.
+	void StartDrawPhaseOnEdtion(int indexEdition);
+
+	// Método que inicia a fase de apuração.
+	void StartCoutingPhaseOnEdition(int indexEdition);
+
+	// Método que inicia a fase de premiação.
+	void StartAwardPhaseOnEdition(int indexEdition);
+
+	// Método que checa as edições anteriores.
+	void CheckPastEditions();
 };
--- src/Source Files/Bookmaker.cpp
+++ src/Source Files/Bookmaker.cpp
@@ -1,9 +1,171 @@
 #include "../Header Files/Bookmaker.h"
 
-Bookmaker::Bookmaker() {}
+Bookmaker::Bookmaker() {
+    editions = vector<Edition>(0);
+}
 
-void Bookmaker::Run() {}
+void Bookmaker::Start() {
+	setlocale(LC_ALL, "portuguese");
+    srand((unsigned)time(NULL));
+    cout << "O programa está sendo iniciado.\n";
+}
 
-void Bookmaker::Start() {}
+void Bookmaker::Run() {
+    int option = 0;
+    int testInt = 0;
+    string testString = "";
 
-void Bookmaker::Finish() {}
+    do {
+        testInt = 0;
+
+        cout << "\n---------------------Menu----------------------\n\n";
+        cout << "Diga a opção que deseja:\n\n";
+        cout << "1 - Iniciar Edição\n";
+        cout << "2 - Checar Edições Anteriores\n";
+        cout << "3 - Encerrar programa\n\n> ";
+        cin >> testString;
+        option = TestException(testString);
+
+        switch (option) {
+        case 1:
+            StartNewEdition();
+            break;
+        case 2:
+            CheckPastEditions();
+            break;
+        case 3:
+            do {
+                cout << "\nVocê tem certeza que deseja encerrar o programa?\n";
+                cout << "1 - Sim\n";
+                cout << "2 - Não\n\n> ";
+                cin >> testString;
+                testInt = TestException(testString);
+
+                if (testInt == 1) {
+                    cout << "\nO programa será finalizado.\n";
+                }
+                else if (testInt == 2) {
+                    cout << "\nO programa voltará ao menu.\n";
+                    option = 0;
+                }
+                else {
+                    cout << "\nEscolha uma opção válida.\n";
+                }
+            } while (testInt != 1 && testInt != 2);
+            break;
+        default:
+            cout << "\nEscolha uma opção válida.\n";
+        }
+        if (option != 3) {
+            cout << "\nPressione Enter para continuar...";
+            cin.ignore();
+            cin.get();
+        }
+    } while (option != 3);
+}
+
+void Bookmaker::Finish() {
+    cout << "\nO programa foi finalizado.\n";
+	exit(0);
+}
+
+int Bookmaker::TestException(string arg_string) {
+    int testInt = 0;
+    string testString = arg_string;
+
+    try {
+        testInt = atoi(testString.c_str());
+        if (testInt <= 0) {
+            throw (testString);
+        }
+    }
+    catch (string testString) {
+        cout << "\nA opção " << testString << " é inválida.\n";
+    }
+    return testInt;
+}
+
+void Bookmaker::StartNewEdition() {
+    int indexEdition = editions.size();
+    editions.resize(indexEdition + 1);
+
+    StartBetPhaseOnEdition(indexEdition);
+}
+
+void Bookmaker::StartBetPhaseOnEdition(int indexEdition) {
+    int option = 0;
+    int testInt = 0;
+    string testString = "";
+    string name = "";
+    string cpf = "";
+
+    do {
+        testInt = 0;
+
+        cout << "\n---------------------Fase de Apostas----------------------\n\n";
+        cout << "Diga a opção que deseja:\n\n";
+        cout << "1 - Registrar uma aposta\n";
+        cout << "2 - Checar a Lista de Apostas\n";
+        cout << "3 - Passar para a fase de sorteio\n\n> ";
+        cin >> testString;
+        option = TestException(testString);
+
+        switch (option) {
+        case 1:
+            cout << "\n---------------------Registro de Aposta----------------------\n";
+            cout << "Qual o nome do apostador?\n\n";
+            cin >> name;
+            cout << "\nQual o cpf do apostador?\n\n";
+            cin >> cpf;
+
+            do {
+                cout << "\nQual forma de registro de aposta deseja fazer?\n";
+                cout << "1 - Escolha Manual\n";
+                cout << "2 - Surpresinha (Escolha Aleatória)\n\n> ";
+                cin >> testString;
+                testInt = TestException(testString);
+                if (testInt == 1) {
+                    int n[5] = { 0 };
+
+                    editions[indexEdition].RegisterNewBet(name, cpf, 1, n);
+                }
+                else if (testInt == 2) {
+                    editions[indexEdition].RegisterNewBet(name, cpf, 2, NULL);
+                }
+                else {
+                    cout << "\nEscolha uma opção válida.\n";
+                }
+            } while (testInt != 1 && testInt != 2);
+            break;
+        case 2:
+            editions[indexEdition].ShowBets();
+            break;
+        case 3:
+            cout << "\nO programa seguirá para a fase de sorteio?\n";
+            break;
+        default:
+            cout << "\nEscolha uma opção válida.\n";
+        }
+        if (option != 3) {
+            cout << "\nPressione Enter para continuar...";
+            cin.ignore();
+            cin.get();
+        }
+    } while (option != 3);
+}
+
+void Bookmaker::StartDrawPhaseOnEdtion(int IndexEdition) {
+
+}
+
+void Bookmaker::StartCoutingPhaseOnEdition(int IndexEdition) {
+
+}
+
+void Bookmaker::StartAwardPhaseOnEdition(int IndexEdition) {
+
+}
+
+void Bookmaker::CheckPastEditions() {
+
+}
